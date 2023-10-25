6203de4d6903488de33ab15ea35c8704cd581a19
dholendar29 <146914063+dholendar29@users.noreply.github.com>
2023-10-25 18:26:41 +0530

    first commit

---
+++ links/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.text import slugify
+# Create your models here.
+class Link(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    url = models.URLField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    clicks = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+    def click(self):
+        self.clicks += 1
+        self.save() 
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
---
+++ links/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Link
+# Create your views here.
+def index(request):
+    links = Link.objects.all()
+    context = {
+        "links" : links
+    }
+    return render(request, 'links/index.html', context=context)
+
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug=link_slug)
+    link.click()
+    return redirect(link.url)
+
+def add_link(request):
+    return render(request,'links/create.html', {})
