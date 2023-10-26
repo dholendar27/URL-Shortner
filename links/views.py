62988cd6128472a12dfee5721fc69140474ac03b
dholendar29 <146914063+dholendar29@users.noreply.github.com>
2023-10-26 09:58:39 +0530

    Added few changes
    Used Modelviews, saving data in the database, uisng cripsy to change the ui

--- links/views.py
+++ links/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from .forms import LinkForm
 from .models import Link
+from django.urls import reverse
 # Create your views here.
 def index(request):
     links = Link.objects.all()
@@ -15,4 +16,15 @@ def root_link(request, link_slug):
     return redirect(link.url)
 
 def add_link(request):
-    return render(request,'links/create.html', {})
+    if request.method == "POST":
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            
+            form.save()
+            return redirect(reverse('home'))
+    else:
+        form = LinkForm()
+    context = {
+        "form":form
+    }
+    return render(request,'links/create.html', context)
